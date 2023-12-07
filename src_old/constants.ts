aedd8aa2773d10202d3c3d27dd71ddff6d2e9463
Micheal Parks <michealparks1989@gmail.com>
2023-12-07 01:17:11 -0500

    cleanup

--- src_old/constants.ts
+++
@@ -1,4 +0,0 @@
-export const defaultMinMax = {
-  max: 1,
-  min: 0,
-}
--- src_old/elements/gizmo.ts
+++
@@ -1,8 +0,0 @@
-import { ViewHelper } from 'trzy'
-import { refs } from '../refs'
-
-export const createViewHelper = () => {
-  const { camera, renderer } = refs
-  const gizmo = new ViewHelper(camera, renderer)
-  return () => gizmo.dispose()
-}
