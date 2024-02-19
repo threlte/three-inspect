5528a94f0f2152344c1babd984143aa0107537f2
Grischa Erbe <grischa.erbe@googlemail.com>
2024-02-19 06:34:33 +0100

    Grid Settings

--- src/lib/extensions/grid/Grid.svelte
+++ src/lib/extensions/grid/Grid.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { Grid } from '@threlte/extras'
 	import { onDestroy } from 'svelte'
+	import {
+		Color,
+		RadioGrid,
+		Slider,
+		type ColorValue,
+		type RadioGridChangeEvent,
+	} from 'svelte-tweakpane-ui'
+	import DropDownPane from '../../components/DropDownPane/DropDownPane.svelte'
 	import ToolbarButton from '../../components/ToolbarButton/ToolbarButton.svelte'
 	import ToolbarItem from '../../components/ToolbarItem/ToolbarItem.svelte'
 	import HorizontalButtonGroup from '../../components/Tools/HorizontalButtonGroup.svelte'
@@ -16,6 +24,9 @@
 		state({ persist }) {
 			return {
 				enabled: persist(true),
+				color: persist('#5f5f5f'),
+				step: persist(1),
+				plane: persist('xz'),
 			}
 		},
 		actions: {
@@ -25,6 +36,15 @@
 			toggleEnabled({ select }) {
 				select((s) => s.enabled).update((enabled) => !enabled)
 			},
+			setColor({ select }, color) {
+				select((s) => s.color).set(color)
+			},
+			setStep({ select }, step) {
+				select((s) => s.step).set(step)
+			},
+			setPlane({ select }, plane) {
+				select((s) => s.plane).set(plane)
+			},
 		},
 	})
 
@@ -33,6 +53,21 @@
 	})
 
 	const enabled = state.select((s) => s.enabled)
+	const color = state.select((s) => s.color)
+	const step = state.select((s) => s.step)
+	const plane = state.select((s) => s.plane)
+
+	const onColorChange = (e: CustomEvent<{ value: ColorValue }>) => {
+		runGridAction('setColor', e.detail.value as string)
+	}
+
+	const onPlaneChange = (e: RadioGridChangeEvent) => {
+		runGridAction('setPlane', e.detail.value as 'xy' | 'xz' | 'zy')
+	}
+
+	const onStepChange = (e: CustomEvent<{ value: number }>) => {
+		runGridAction('setStep', e.detail.value)
+	}
 </script>
 
 <ToolbarItem position="left">
@@ -47,19 +82,27 @@
 			tooltip="Grid"
 		/>
 
-		<!-- <DropDownPane title="Settings">
-			<Checkbox
-				value={true}
-				on:change={(e) => {
-					if (e.detail.value) {
-						run('enable')
-					} else {
-						run('disable')
-					}
-				}}
-				label="Enabled"
+		<DropDownPane title="Grid Settings">
+			<Color
+				value={$color}
+				label="Color"
+				on:change={onColorChange}
 			/>
-		</DropDownPane> -->
+
+			<Slider
+				value={$step}
+				label="Step"
+				min={0}
+				on:change={onStepChange}
+			/>
+			<RadioGrid
+				value={$plane}
+				values={['xy', 'xz', 'yz']}
+				rows={1}
+				label="Plane"
+				on:change={onPlaneChange}
+			/>
+		</DropDownPane>
 	</HorizontalButtonGroup>
 </ToolbarItem>
 
@@ -73,8 +116,10 @@
 		}}
 		userData={{ ignoreOverrideMaterial: true }}
 		infiniteGrid
-		cellSize={1}
+		cellSize={$step}
+		sectionColor={$color}
+		cellColor={$color}
+		plane={$plane}
 		renderOrder={9999}
-		plane={'xz'}
 	/>
 {/if}
--- src/lib/extensions/grid/types.ts
+++ src/lib/extensions/grid/types.ts
@@ -2,9 +2,15 @@ export const gridScope = 'grid'
 
 export type GridState = {
 	enabled: boolean
+	color: string
+	step: number
+	plane: 'xy' | 'xz' | 'zy'
 }
 
 export type GridActions = {
 	toggleEnabled: () => void
 	setEnabled: (enabled: boolean) => void
+	setColor: (color: string) => void
+	setStep: (step: number) => void
+	setPlane: (plane: 'xy' | 'xz' | 'zy') => void
 }
