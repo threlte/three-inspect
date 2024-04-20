8b5e81e6d9ab4a1e4eb020551f7fc41fdfc5aac4
Grischa Erbe <grischa.erbe@googlemail.com>
2024-04-21 00:22:27 +0200

    Deployment stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
-import { studioPlugin } from './src/lib/plugin'
+// import { studioPlugin } from './src/lib/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [Inspect(), studioPlugin(), sveltekit()],
-	build: {
-		minify: true,
-		target: 'esnext',
-	},
+	plugins: [Inspect(), sveltekit()],
+	// plugins: [Inspect(), studioPlugin(), sveltekit()],
+	// build: {
+	// 	minify: true,
+	// 	target: 'esnext',
+	// },
 	server: {
 		port: 5000,
 	},
