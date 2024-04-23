c226183738a2a3b09ee707c1823fcb261050bb56
Grischa Erbe <grischa.erbe@googlemail.com>
2024-04-24 00:05:02 +0200

    pre-task

--- src/routes/simple/Scene.svelte
+++ src/routes/simple/Scene.svelte
@@ -5,21 +5,23 @@
 
 <!-- CAMERA -->
 <T.PerspectiveCamera
-  position={[0, 0, 10]}
+  position={[-8.9479,4.2,6.5803]}
   makeDefault
+  fov={24.75}
+  rotation={[-0.509,-0.8405,-0.394]}
 />
 
 <!-- LIGHTS -->
-<T.DirectionalLight />
-<T.AmbientLight />
+<T.DirectionalLight position={[-2.5,8,3.1]} castShadow color="#fcbaba" />
+<T.AmbientLight color="#e1e5ff" />
 
 <!-- CUBE -->
-<T.Mesh>
+<T.Mesh position={[0,0.9,0]} castShadow>
   <RoundedBoxGeometry radius={0.1} />
   <T.MeshStandardMaterial color="red" />
 </T.Mesh>
 
-<T.Mesh>
+<T.Mesh receiveShadow>
   <T.CylinderGeometry args={[2, 2, 0.2, 128]} />
   <T.MeshStandardMaterial />
 </T.Mesh>
