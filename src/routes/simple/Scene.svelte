b95ab3fd4e18b5ea31faf1abaaa4a99f4c5e8f83
Grischa Erbe <grischa.erbe@googlemail.com>
2024-04-24 00:32:31 +0200

    after rotation

--- src/routes/simple/Scene.svelte
+++ src/routes/simple/Scene.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
+  import { T, useTask } from '@threlte/core'
   import { RoundedBoxGeometry } from '@threlte/extras'
+
+  let rotationY = $state(0)
+
+  useTask((delta) => {
+    rotationY += 0.2 * delta
+  })
 </script>
 
 <!-- CAMERA -->
 <T.PerspectiveCamera
-  position={[-8.9479,4.2,6.5803]}
+  position={[-8.9479, 4.2, 6.5803]}
   makeDefault
   fov={24.75}
-  rotation={[-0.509,-0.8405,-0.394]}
+  rotation={[-0.509, -0.8405, -0.394]}
 />
 
 <!-- LIGHTS -->
-<T.DirectionalLight position={[-2.5,8,3.1]} castShadow color="#fcbaba" />
+<T.DirectionalLight
+  position={[-2.5, 8, 3.1]}
+  castShadow
+  color="#fcbaba"
+/>
 <T.AmbientLight color="#e1e5ff" />
 
 <!-- CUBE -->
-<T.Mesh position={[0,0.9,0]} castShadow>
+<T.Mesh
+  position={[0, 0.9, 0]}
+  castShadow
+  rotation.y={rotationY}
+>
   <RoundedBoxGeometry radius={0.1} />
   <T.MeshStandardMaterial color="red" />
 </T.Mesh>
